94796d21403e2f0d0c16b0a104f9cd5616f43edf
ankita938 <ankitadubey1232002@gmail.com>
2024-06-26 23:49:53 +0530

    added

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.css';
+import { ImSpoonKnife } from "react-icons/im";
+
+function Header() {
+  return (
+    <div className="header">
+
+    <br/>
+     <div className='login'>
+        <button>Login</button>
+        <button>signup</button>
+    </div>
+    {/* <div className='spoon'>
+    <ImSpoonKnife />
+    </div> */}
+    <div className='Recipe'>
+        <div className='spoon'>
+            <ImSpoonKnife />
+        </div>
+        <h1>Recipe Act ReactJs</h1>
+        <p>Let's have fun building this app together...</p>
+    </div>
+    </div>
+  );
+}
+
+export default Header;
+
